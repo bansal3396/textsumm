c659ec4352e3b3326ebb795ffbad53731801e422
bansal3396 <bansaludhav3396@gmail.com>
2025-11-26 10:44:42 +0530

    Create summ.py

---
+++ summ.py
@@ -0,0 +1,13 @@
+﻿from utils import clean_text
+
+def summarize(text):
+    cleaned = clean_text(text)
+    sentences = cleaned.split('.')
+    sentences = [s.strip() for s in sentences if s.strip()]
+    if len(sentences) > 2:
+        return sentences[0] + '. ' + sentences[-1] + '.'
+    return cleaned
+
+if __name__ == '__main__':
+    sample = 'Git helps developers track changes. It is fast and reliable. It also supports branching.'
+    print(summarize(sample))
